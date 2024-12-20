ca9e9378af96bec07a9daf44eaf0bbf83672bbe8
mada.petru <mada.petru@digitalexplorer.ro>
2024-12-20 20:30:40 +0200

    fix diacritics problem

--- agro-admin-api/src/main/java/ro/adi/farming_land_operation_history/dto/common/PlantTypeDto.java
+++ agro-admin-api/src/main/java/ro/adi/farming_land_operation_history/dto/common/PlantTypeDto.java
@@ -1,5 +1,5 @@
 package ro.adi.farming_land_operation_history.dto.common;
 
 public enum PlantTypeDto {
-    NONE, PORUMB, GRÂU, ORZ, OVĂZ, LUCERNĂ, TRIFOI, ORZOAICĂ
+    NONE, PORUMB, GRAU, ORZ, OVAZ, LUCERNA, TRIFOI, ORZOAICA
 }
--- agro-admin-app/src/main/java/ro/adi/agroadmin/common/entity/PlantType.java
+++ agro-admin-app/src/main/java/ro/adi/agroadmin/common/entity/PlantType.java
@@ -1,5 +1,5 @@
 package ro.adi.agroadmin.common.entity;
 
 public enum PlantType {
-    NONE, PORUMB, GRÂU, ORZ, OVĂZ, LUCERNĂ, TRIFOI, ORZOAICĂ
+    NONE, PORUMB, GRAU, ORZ, OVAZ, LUCERNA, TRIFOI, ORZOAICA
 }
