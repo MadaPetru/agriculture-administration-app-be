b58dfe3eab0e96cbafd7f47268202b58dffb5b5e
mada.petru <mada.petru@digitalexplorer.ro>
2024-12-25 16:43:46 +0200

    Revert "revert this"
    This reverts commit cb8edb0a6eeccd87326de9bfa151511253920461.

--- agro-admin-app/src/main/java/ro/adi/agroadmin/AgricultureAdministrationAppBeApplication.java
+++ agro-admin-app/src/main/java/ro/adi/agroadmin/AgricultureAdministrationAppBeApplication.java
@@ -1,7 +1,6 @@
 package ro.adi.agroadmin;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
