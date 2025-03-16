578244deca723429825df12b2b3e33dc78940ead
mada.petru <mada.petru@digitalexplorer.ro>
2025-03-16 17:44:32 +0200

    remove workflow deploy azure and add new operation type

--- agro-admin-api/src/main/java/ro/adi/farming_land_operation_history/dto/common/OperationTypeDto.java
+++ agro-admin-api/src/main/java/ro/adi/farming_land_operation_history/dto/common/OperationTypeDto.java
@@ -1,5 +1,5 @@
 package ro.adi.farming_land_operation_history.dto.common;
 
 public enum OperationTypeDto {
-    ARAT, SEMANAT, RECOLTARE, DISCUIT, SCARIFICAT, IERBICIDAT
+    ARAT, SEMANAT, RECOLTARE, DISCUIT, SCARIFICAT, IERBICIDAT, INGRASAMINTE, COMBINATOR
 }
